34bd1581f7dca3b36fe06b93932738ba8b517ace
Janne Suotsalo <janne.suotsalo@gmail.com>
2020-03-22 17:31:43 +0200

    working local strategy

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 "use strict";
 const express = require("express");
 const cors = require("cors");
+const passport = require("./utils/pass");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 3000;
 
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect("/form");
+  }
+};
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/form" }),
+  (req, res) => {
+    console.log("success");
+    res.redirect("/secret");
+  }
+);
+
+app.get("/secret", loggedIn, (req, res) => {
+  res.render("secret");
+});
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
 let cats = require("./routes/catRoute");
 let users = require("./routes/userRoute");
---
+++ utils/pass.js
@@ -0,0 +1,79 @@
+"use strict";
+const passport = require("passport");
+const Strategy = require("passport-local").Strategy;
+
+// fake database: ****************
+const users = [
+  {
+    user_id: 1,
+    name: "Foo Bar",
+    email: "foo@bar.fi",
+    password: "foobar"
+  },
+  {
+    user_id: 2,
+    name: "Bar Foo",
+    email: "bar@foo.fi",
+    password: "barfoo"
+  }
+];
+// *******************
+
+// fake database functions *********
+const getUser = id => {
+  const user = users.filter(usr => {
+    if (usr.user_id === id) {
+      return usr;
+    }
+  });
+  return user[0];
+};
+
+const getUserLogin = email => {
+  console.log("email is: " + email);
+  const user = users.filter(usr => {
+    console.log(usr.email);
+    console.log(email);
+    if (usr.email === email) {
+      console.log("user is: " + usr.name);
+      return usr;
+    }
+  });
+  return user[0];
+};
+// *****************
+
+// serialize: store user id in session
+passport.serializeUser((id, done) => {
+  console.log("serialize", id);
+  done(null, id);
+});
+
+// deserialize: get user id from session and get all user data
+passport.deserializeUser(async (id, done) => {
+  const user = getUser(id);
+  console.log("deserialize", user);
+  done(null, user);
+});
+
+passport.use(
+  new Strategy(async (username, password, done) => {
+    const params = username;
+    console.log(params);
+    try {
+      const user = await getUserLogin(params);
+      console.log("Local strategy", user); // result is binary row
+      if (user === undefined) {
+        return done(null, false, { message: "Incorrect email." });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user.user_id);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+module.exports = passport;
