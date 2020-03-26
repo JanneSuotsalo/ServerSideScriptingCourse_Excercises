178d06a85dbbcc50429fa8e3ae3644d7a383ddb9
Janne Suotsalo <janne.suotsalo@gmail.com>
2020-03-26 21:34:39 +0200

    connecting to db

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 "use strict";
+require("dotenv").config();
+
 const express = require("express");
+const db = require("./model/db");
 const cors = require("cors");
 const passport = require("./utils/pass");
 const app = express();
@@ -46,4 +49,6 @@ app.use("/cat", passport.authenticate("jwt", { session: false }), cats);
 app.use("/user", passport.authenticate("jwt", { session: false }), users);
 app.use("/auth", authRoutes);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+db.on("connected", () => {
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+});
